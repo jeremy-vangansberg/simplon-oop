05a14efc12b2647f90a6ffa223b733dd69239bbc
jeremy Vangansberg <jeremy.vangansberg@gmail.com>
2023-10-11 12:01:21 +0200

    merging correction in the main branch

--- correction/app1_geometry_game/main2.py
+++ correction/app1_geometry_game/main2.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 class Point:
 
     def __init__(self, x, y):
@@ -20,14 +21,10 @@ class Rectangle:
         self.point1 = point1
         self.point2 = point2
 
-    def area(self):
-        return (self.point2.x - self.point1.x) * \
-               (self.point2.y - self.point1.y)
-
 
 # Create rectangle object
 rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
-              Point(randint(10, 19), randint(10, 19)))
+                      Point(randint(10, 19), randint(10, 19)))
 
 # Print rectangle coordinates
 print("Rectangle Coordinates: ",
@@ -38,8 +35,7 @@ print("Rectangle Coordinates: ",
 
 # Get point and area from user
 user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
-user_area = float(input("Guess rectangle area: "))
 
 # Print out the game result
-print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
-print("Your area was off by: ", rectangle.area() - user_area)
+print("Your point was inside rectangle: ",
+      user_point.falls_in_rectangle(rectangle))
